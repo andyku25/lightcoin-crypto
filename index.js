65a91868ef51f7b7dfbcaedb68cee528f32fc6e0
Andrew <andrew.ku93@gmail.com>
2021-02-13 01:14:16 +0000

    modify the class implementation of Account, Transaction, Withdrawl and Deposit

--- index.js
+++ index.js
@@ -1,29 +1,91 @@
-let balance = 500.00;
+// HELPER FUNCTION
 
-class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
+// CREATE AN ACCOUNT OBJECT
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+  get balance() {
+    let balance = 0;
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
   }
 
 }
 
+// CREATE A TRANSACTION SUPERCLASS TO (WITHDRAWALS AND DEPOSITS)
+class Transaction {
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+    } else {
+      console.log("Transaction could not be processed.");
+    }
+  }
+
+}
+
+// CREATE A WITHDRAWAL OBJECT
+class Withdrawal extends Transaction {
+  get value() {
+    return -this.amount;
+  }
+
+  // CHECK IF BALANCE WILL BE NEGATIVE IF WITHDRAWAL GOES THROUGH
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+// CREATE A DEPOSIT OBJECT
+class Deposit extends Transaction {
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    return true;
+  }
+}
 
 
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+console.log(`Starting Balance:  $${myAccount.balance}`);
+
+t1 = new Withdrawal(50.25 , myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log(`Balance after transaction 1: $${myAccount.balance}`);
+console.log("------------------------");
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log(`Balance after transaction 2: $${myAccount.balance}`);
+console.log("------------------------");
 
-console.log('Balance:', balance);
+t3 = new Deposit(120.00, myAccount);
+t3.commit();
+console.log("Transaction 3:", t3);
+console.log(`Balance after transaction 3: $${myAccount.balance}`);
+console.log("------------------------");
